b8a12795d562e9f01bcdd047c31a88496f38330b
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-24 13:40:52 -0500

    Chapter 2 Exercise 3

---
+++ condense_dweight.c
@@ -0,0 +1,20 @@
+/*
+C Programming - A Modern Approach
+Exercises - 3. Condense the dweight.c program by (1) replacinng the assignments
+To height, length and width with initializers and (2) removing the weight variable
+Instead calculating (volume + 165) / 166 within the last printf
+*/
+//Date: 02/24/25
+//Author: VV - VesperVanity - https://github.com/VesperVanity
+#include <stdio.h>
+
+int main(void)
+{
+	int height = 100;
+	int length = 20;
+	int width = 40;
+	int volume = height * length * width;
+
+	printf("%s%d\n", "Volume: ", (volume + 165) / 166);
+	return 0;
+}
