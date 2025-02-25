2d8255a33a116b37ae47ab13943d3e5a713afd33
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-25 10:30:07 -0500

    Chapter 3 Project 1

---
+++ date_form.c
@@ -0,0 +1,22 @@
+/*
+C Programming - A Modern Approach
+Chapter 3 Programming Projects - 1. Write a program that
+accepts a date from the user in the form mm/dd/yyyy and
+then displays it in the form yyyymmdd
+*/
+//////////////////////////////////////////////////////////////////////////////////
+//Date: 02/25/25
+//Author: VV - VesperVanity - https://github.com/VesperVanity
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "Type a date with format mm/dd/yyyy: ");
+	int month = 0;
+	int day = 0;
+	int year = 0;
+	scanf("%d%d%d", &month, &day, &year);
+	printf("%s%d %s%d %s%d\n", "Year: ", year, " Month: ", month, " Day: ", day);
+	return 0;
+}
