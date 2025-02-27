4d9c8fa5314a88a7dcd3706f22e5a285590804d6
VesperVanity <OCEAN.Jung@protonmail.com>
2025-02-27 10:22:10 -0500

    Re-init

---
+++ chapter_5/departure_time.c
@@ -0,0 +1,21 @@
+/*
+C Programming - A Modern Approach
+Chapter 5 Programming Projects - 8. Write a program that
+asks the user to enter a time (expressed in hours and minutes,
+using the 24-hour clock)
+The program then displays the departure and arrival times
+for the flight whose depature time is closest to that entered
+by the user
+*/
+//////////////////////////////////////////////////////////////////////////////////
+//Date: 02/27/25
+//Author: VV - VesperVanity - https://github.com/VesperVanity
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s", "Enter a time (hh:mm) military format: ");
+	
+	return 0;
+}
